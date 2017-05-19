e79479ad030e626fd6edbc57bae79495abf9facd
camelia <camelia0858@gmail.com>
2017-05-19 16:14:00 +0900

    all crawler available

--- crawlers/crawler/csck2notice.py
+++ crawlers/crawler/csck2notice.py
@@ -9,15 +9,19 @@ base_url = 'http://csck2.hanyang.ac.kr/'
 driver.get('http://csck2.hanyang.ac.kr/front/community/notice')
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
-notices = soup.select('')
+hrefs = soup.select('ul.board-default-list > li > a')
+notices = soup.select('ul.board-default-list > li > a > span.subject')
+dates = soup.select('ul.board-default-list > li > a > span.datetime')
+
 driver.close()
 
 # idx : PK
 # title : n.text.strip()
 # link : base_url + n['href']
 # TODO : make proper seperator
-for n in notices:
-    idx = int((str(n['href']).split("idx=",1)[1]).split("&page",1)[0])
+for n,h,d in zip(notices,hrefs,dates):
+    idx = int((str(h['href']).split("id=",1)[1]))
     print(idx)
-    print(n.text.strip())
-    print(base_url + n['href'])
+    print((n.text)[3:].strip())
+    print(base_url + h['href'])
+    print(d.text.strip())
