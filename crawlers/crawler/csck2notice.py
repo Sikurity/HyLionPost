7f16e93c9d70dae63d11f3d9bdeedf8412f61f5e
camelia <camelia0858@gmail.com>
2017-05-25 03:21:08 +0900

    crawler들 함수화 시킴

--- crawlers/crawler/csck2notice.py
+++ crawlers/crawler/csck2notice.py
@@ -1,27 +1,26 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-# TODO : change hardcoded path to relative path
-driver = webdriver.Chrome('/Users/camelia/HyLionPost/crawlers/res/chromedriver')
-driver.implicitly_wait(3)
+#driver : chromedriver
+def csck2notice(driver):
+    driver.implicitly_wait(3)
+    base_url = 'http://csck2.hanyang.ac.kr/'
+    driver.get('http://csck2.hanyang.ac.kr/front/community/notice')
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    hrefs = soup.select('ul.board-default-list > li > a')
+    notices = soup.select('ul.board-default-list > li > a > span.subject')
+    dates = soup.select('ul.board-default-list > li > a > span.datetime')
 
-base_url = 'http://csck2.hanyang.ac.kr/'
-driver.get('http://csck2.hanyang.ac.kr/front/community/notice')
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-hrefs = soup.select('ul.board-default-list > li > a')
-notices = soup.select('ul.board-default-list > li > a > span.subject')
-dates = soup.select('ul.board-default-list > li > a > span.datetime')
+    driver.close()
 
-driver.close()
-
-# idx : PK
-# title : n.text.strip()
-# link : base_url + n['href']
-# TODO : make proper seperator
-for n,h,d in zip(notices,hrefs,dates):
-    idx = int((str(h['href']).split("id=",1)[1]))
-    print(idx)
-    print((n.text)[3:].strip())
-    print(base_url + h['href'])
-    print(d.text.strip())
+    # idx : PK
+    # title : n.text.strip()
+    # link : base_url + n['href']
+    # TODO : make proper seperator
+    for n,h,d in zip(notices,hrefs,dates):
+        idx = int((str(h['href']).split("id=",1)[1]))
+        print(idx)
+        print((n.text)[3:].strip())
+        print(base_url + h['href'])
+        print(d.text.strip())
--- crawlers/crawler/csstrk.py
+++ crawlers/crawler/csstrk.py
@@ -1,25 +1,24 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-# TODO : change hardcoded path to relative path
-driver = webdriver.Chrome('/Users/camelia/HyLionPost/crawlers/res/chromedriver')
-driver.implicitly_wait(3)
+def csstrk(driver):
+    driver.implicitly_wait(3)
 
-base_url = 'http://cs.hanyang.ac.kr'
-driver.get('http://cs.hanyang.ac.kr/board/trk_board.php')
-html = driver.page_source
-soup = BeautifulSoup(html, 'html.parser')
-notices = soup.select('table.bbs_con > tbody > tr > td > a:nth-of-type(1)')
-dates = soup.select('table.bbs_con > tbody > tr > td:nth-of-type(5)')
+    base_url = 'http://cs.hanyang.ac.kr'
+    driver.get('http://cs.hanyang.ac.kr/board/trk_board.php')
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    notices = soup.select('table.bbs_con > tbody > tr > td > a:nth-of-type(1)')
+    dates = soup.select('table.bbs_con > tbody > tr > td:nth-of-type(5)')
 
-driver.close()
+    driver.close()
 
-# idx : PK
-# title : n.text.strip()
-# link : base_url + n['href']
-# TODO : make proper seperator
-for n in notices:
-    idx = int((str(n['href']).split("idx=",1)[1]).split("&page",1)[0])
-    print(idx)
-    print(n.text.strip())
-    print(base_url + n['href'])
+    # idx : PK
+    # title : n.text.strip()
+    # link : base_url + n['href']
+    # TODO : make proper seperator
+    for n in notices:
+        idx = int((str(n['href']).split("idx=",1)[1]).split("&page",1)[0])
+        print(idx)
+        print(n.text.strip())
+        print(base_url + n['href'])
