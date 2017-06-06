92e8beaf217b6d203f4fead3dbbdcac427b0abf7
PineApple777 <metaljsw2@naver.com>
2017-06-07 02:05:01 +0900

    make new post filtering & notification

--- server/controllers/generate.py
+++ server/controllers/generate.py
@@ -17,56 +17,66 @@ import time
 from dbconn import *
 import pyrebase
 
+# notification part
+from pushnotify import *
+
 crawl = '../crawlers/crawler/result/'
 
 # crawler and server connection part
-def csck2notice_server(): 
-    csck2notice(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
-    data = codecs.open(crawl + 'csck2notice.json', 'r', encoding ='utf-8')
-    datas = json.load(data)
-    old_data = load_lionbase("csjob")
-    print (old_data.key())
-    push_lionbase(datas,"csck2notice")
-    return datas
+def csck2notice_server():
+    while True :
+        try:
+            csck2notice(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
+            data = codecs.open(crawl + 'csck2notice.json', 'r', encoding ='utf-8')
+            new_datas = json.load(data)
+            old_data = load_lionbase("csck2notice").val()
+            filter_notify_func("csck2notice",new_datas,old_data)
+            print('[CSCK2NOTICE] server waiting...\n')
+            time.sleep(15)
+        except JSONDecodeError as s :
+            print('thread error')
+            print(s)
+            sys.exit()
+
 def csjob_server():
     while True :
         csjob(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver')) 
         data = codecs.open(crawl + '/csjob.json', 'r', encoding ='utf-8')
-        datas = json.load(data)
+        new_datas = json.load(data)
         old_data = load_lionbase("csjob").val()
-        if old_data :
-            first_dat = old_data[0]
-            for board in old_data :
-                if board['inner_idx'] == datas[0]['inner_idx'] :
-                    if first_dat['inner_idx'] == board['inner_idx'] :
-                        print('[CSJOB] No board lists are not updated...')
-                        break
-                    else :
-                        print('[CSJOB] New posts updated!!')
-                        print(board)
-        else:
-            print('first input!!\n')
-            push_lionbase(datas,"csjob")
-        print('server waiting...\n')
-        time.sleep(5)
+        filter_notify_func("csjob",new_datas,old_data)
+        print('[CSJOB] server waiting...\n')
+        time.sleep(15)
 
 def csgradu_server():
-    csgradu(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
-    data = codecs.open(crawl + '/csgradu.json', 'r', encoding ='utf-8')
-    datas = json.load(data)
-    return datas
+    while True :
+        csgradu(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
+        data = codecs.open(crawl + '/csgradu.json', 'r', encoding ='utf-8')
+        new_datas = json.load(data)
+        old_data = load_lionbase("csgradu").val()
+        filter_notify_func("csgradu",new_datas, old_data)
+        print('[CSGRADU]server waiting...')
+        time.sleep(15)
 
 def csnotice_server():
-    csnotice(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
-    data = codecs.open(crawl + '/csnotice.json', 'r', encoding ='utf-8')
-    datas = json.load(data)
-    return datas
+    while True :
+        csnotice(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
+        data = codecs.open(crawl + '/csnotice.json', 'r', encoding ='utf-8')
+        new_datas = json.load(data)
+        old_data = load_lionbase("csnotice").val()
+        filter_notify_func("csnotice",new_datas, old_data)
+        print('[CSNOTICE] server waiting...')
+        time.sleep(15)
 
 def csstrk_server():
-    csstrk(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
-    data = codecs.open(crawl + '/csstrk.json', 'r', encoding ='utf-8')
-    datas = json.load(data)
-    return datas
+    while True :
+        csstrk(webdriver.Chrome('/Users/Jungsunwook/HyLionPost/crawlers/res/chromedriver'))
+        data = codecs.open(crawl + '/csstrk.json', 'r', encoding ='utf-8')
+        new_datas = json.load(data)
+        old_data = load_lionbase("csstrk").val()
+        filter_notify_func("csstrk",new_datas, old_data)
+        print('[CSSTRK] server waiting...')
+        time.sleep(15)
 
 # test crawler part
 def demo_server():
@@ -75,31 +85,7 @@ def demo_server():
         data = codecs.open( crawl + '/demon.json', 'r', encoding = 'utf-8' )
         new_datas = json.load(data)
         old_data = load_lionbase("demon").val()
-        if old_data :
-            new_datas = sorted(new_datas, key=lambda tmp : int(tmp['inner_idx']))
-            print(len(old_data))
-            update_data = old_data[len(old_data)-1]['inner_idx']
-            last_data = old_data[len(old_data)-1]
-            for board in new_datas :
-                if board['inner_idx'] != old_data[len(old_data)-1]['inner_idx'] :
-                    continue
-                else :
-                    if last_data['inner_idx'] == new_datas[len(new_datas)-1]['inner_idx'] :
-                        print('[DEMO] No post update!!')
-                        break
-                    else :
-                        print(update_data)
-                        for x in range(len(old_data),len(new_datas)) :
-                            value = {}
-                            value[str(x)] = new_datas[x]
-                            print('[DEMO] New data was update')
-                            update_firebase(value,"demon")
-                            print('[DEMO] New data updated in db')
-                        break
-        else:
-            new_datas = sorted(new_datas, key=lambda tmp : int(tmp['inner_idx']))
-            print('first input!!')
-            push_lionbase(new_datas,"demon")
-        print('server waiting...')
-        time.sleep(10)
+        filter_notify_func("demon",new_datas, old_data)
+        print('[DEMON] server waiting...')
+        time.sleep(15)
 
--- server/dbconn.py
+++ server/dbconn.py
@@ -1,6 +1,7 @@
 #firebase part 
 import pyrebase
 from controllers.lionbase import *
+from pushnotify import *
 
 # DB Firebase part
 def connect_firebase():
@@ -25,3 +26,42 @@ def load_lionbase(name):
 def update_firebase(data,name):
     db = connect_firebase()
     db.child("board_datas").child(name).update(data)
+
+
+# data filter part
+    
+def filter_notify_func(name, new_datas, old_data) :
+    if old_data :
+        new_datas = sorted(new_datas, key=lambda tmp : int(tmp['inner_idx']))
+        print('[',name.upper(),'] In db, There are ',len(old_data), ' messages')
+        update_data = old_data[len(old_data)-1]['inner_idx']
+        last_data = old_data[len(old_data)-1]
+        for board in new_datas :
+            if board['inner_idx'] != old_data[len(old_data)-1]['inner_idx'] :
+                continue
+            else :
+                if last_data['inner_idx'] == new_datas[len(new_datas)-1]['inner_idx'] :
+                    print('[',name.upper(),'] No post update!!')
+                    break
+                else :
+                    print(update_data)
+                    for x in range(len(old_data),len(new_datas)) :
+                        value = {}
+                        value[str(x)] = new_datas[x]
+                        print('[',name.upper(),'] New data was update')
+                        update_firebase(value,name)
+                        print('[',name.upper(),'] New data updated in db')
+                        result = pyfcm_notify(name,new_datas[x]['title'])
+                        if result == 0 :
+                            print('push notifitcation error')
+                            sys.exit()
+                        else :
+                            print('notification success')
+                    break
+    else:
+        new_datas = sorted(new_datas, key=lambda tmp : int(tmp['inner_idx']))
+        print('first input!!')
+        push_lionbase(new_datas, name)
+
+
+
