b34c3aa537a19ee541579351469e43b6e704a042
camelia <camelia0858@gmail.com>
2017-06-07 10:53:54 +0900

    remove useless

--- crawlers/crawlserver/crawlenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/camelia/SoftwareStudio/HyLionPost/crawlers/crawlserver/crawlenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
